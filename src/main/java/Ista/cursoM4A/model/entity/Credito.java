827549bb871bc49f8d2f44bea76ee052caa28aa7
ErickGuarango <124636559+ErickGuarango@users.noreply.github.com>
2024-05-31 10:37:15 -0500

    Creacion de clase negocio,credito

---
+++ src/main/java/Ista/cursoM4A/model/entity/Credito.java
@@ -0,0 +1,128 @@
+package Ista.cursoM4A.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "Credito")
+public class Credito implements Serializable{
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_socicitud;
+	
+	private String Plazo;
+	private double Valor_solicitado;
+	private String Privincia;
+	private String Ciudad;
+	private String Tipo_credito;
+	private String Estado;
+	private String Forma_pago;
+	private String Inversion;
+	
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "fecha_creaccion")
+	@Temporal(TemporalType.DATE)
+	private Date fecha_creaccion;
+	
+	@PrePersist
+	public void prePersist() {
+		fecha_creaccion = new Date();
+	}
+
+	public String getPlazo() {
+		return Plazo;
+	}
+
+	public void setPlazo(String plazo) {
+		Plazo = plazo;
+	}
+
+	public double getValor_solicitado() {
+		return Valor_solicitado;
+	}
+
+	public void setValor_solicitado(double valor_solicitado) {
+		Valor_solicitado = valor_solicitado;
+	}
+
+	public String getPrivincia() {
+		return Privincia;
+	}
+
+	public void setPrivincia(String privincia) {
+		Privincia = privincia;
+	}
+
+	public String getCiudad() {
+		return Ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		Ciudad = ciudad;
+	}
+
+	public String getTipo_credito() {
+		return Tipo_credito;
+	}
+
+	public void setTipo_credito(String tipo_credito) {
+		Tipo_credito = tipo_credito;
+	}
+
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+
+	public String getForma_pago() {
+		return Forma_pago;
+	}
+
+	public void setForma_pago(String forma_pago) {
+		Forma_pago = forma_pago;
+	}
+
+	public String getInversion() {
+		return Inversion;
+	}
+
+	public void setInversion(String inversion) {
+		Inversion = inversion;
+	}
+
+	public Date getFecha_creaccion() {
+		return fecha_creaccion;
+	}
+
+	public void setFecha_creaccion(Date fecha_creaccion) {
+		this.fecha_creaccion = fecha_creaccion;
+	}
+
+	public Long getId_socicitud() {
+		return id_socicitud;
+	}
+
+	public void setId_socicitud(Long id_socicitud) {
+		this.id_socicitud = id_socicitud;
+	}
+	
+	
+
+}
--- src/main/java/Ista/cursoM4A/model/entity/Negocio.java
+++ src/main/java/Ista/cursoM4A/model/entity/Negocio.java
@@ -2,6 +2,142 @@ package Ista.cursoM4A.model.entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Negocio")
 public class Negocio implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_negocio;
+	
+	public Long getId_negocio() {
+		return id_negocio;
+	}
+	public void setId_negocio(Long id_negocio) {
+		this.id_negocio = id_negocio;
+	}
+	public String getActividad() {
+		return Actividad;
+	}
+	public void setActividad(String actividad) {
+		Actividad = actividad;
+	}
+	@Column(unique = true)
+	private int Ruc;
+
+	private String Razon_Social;
+	private String Actividad;
+	private String Sub_actividad;
+	private int Num_empleados;
+	private int Antiguedad;
+	private String Lugar_ventas;
+	private String Tiempo;
+	private boolean hipotecado;
+	private String Ciudad;
+	private String Direccion;
+	private int Num_casa;
+	private String Referencia;
+	private int Num_telefono;
+	
+	
+	
+	
+	public int getRuc() {
+		return Ruc;
+	}
+	public void setRuc(int ruc) {
+		Ruc = ruc;
+	}
+	public String getRazon_Social() {
+		return Razon_Social;
+	}
+	public void setRazon_Social(String razon_Social) {
+		Razon_Social = razon_Social;
+	}
+	public String getSub_actividad() {
+		return Sub_actividad;
+	}
+	public void setSub_actividad(String sub_actividad) {
+		Sub_actividad = sub_actividad;
+	}
+	public int getNum_empleados() {
+		return Num_empleados;
+	}
+	public void setNum_empleados(int num_empleados) {
+		Num_empleados = num_empleados;
+	}
+	public int getAntiguedad() {
+		return Antiguedad;
+	}
+	public void setAntiguedad(int antiguedad) {
+		Antiguedad = antiguedad;
+	}
+	public String getLugar_ventas() {
+		return Lugar_ventas;
+	}
+	public void setLugar_ventas(String lugar_ventas) {
+		Lugar_ventas = lugar_ventas;
+	}
+	public String getTiempo() {
+		return Tiempo;
+	}
+	public void setTiempo(String tiempo) {
+		Tiempo = tiempo;
+	}
+	public boolean isHipotecado() {
+		return hipotecado;
+	}
+	public void setHipotecado(boolean hipotecado) {
+		this.hipotecado = hipotecado;
+	}
+	public String getCiudad() {
+		return Ciudad;
+	}
+	public void setCiudad(String ciudad) {
+		Ciudad = ciudad;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	public int getNum_casa() {
+		return Num_casa;
+	}
+	public void setNum_casa(int num_casa) {
+		Num_casa = num_casa;
+	}
+	public String getReferencia() {
+		return Referencia;
+	}
+	public void setReferencia(String referencia) {
+		Referencia = referencia;
+	}
+	public int getNum_telefono() {
+		return Num_telefono;
+	}
+	public void setNum_telefono(int num_telefono) {
+		Num_telefono = num_telefono;
+	}
+	
+	 
+	
+	
+	
+	
+	
 
 }
